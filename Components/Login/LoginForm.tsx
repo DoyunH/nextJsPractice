4008e46bdbe2badc2ab11cb6fdb68ab45903fef4
Doyun <doyun@talktomeinkorean.com>
2023-02-15 11:24:14 +0900

    feature: login page front setting

---
+++ Components/Login/LoginForm.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+interface LoginFormProps {
+  onSubmit: (email: string, password: string) => Promise<void>;
+}
+
+export default function LoginForm({ onSubmit }: LoginFormProps) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await onSubmit(email, password);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col justify-center items-center"
+    >
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="mt-2 mb-2 p-2 border border-gray-300 rounded-md"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="mb-2 p-2 border border-gray-300 rounded-md"
+      />
+      <button
+        type="submit"
+        className="mb-2 p-2 bg-amber-400 text-white rounded-md"
+      >
+        Login
+      </button>
+      {message && <p>{message}</p>}
+    </form>
+  );
+}
