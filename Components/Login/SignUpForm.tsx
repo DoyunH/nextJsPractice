e7b419a381c9ede8a50160c2d5b68fad3417d206
Doyun <doyun@talktomeinkorean.com>
2023-03-02 16:59:51 +0900

    feature: Sign up 라이브러리 분리

--- Components/Login/SignUpForm.tsx
+++ Components/Login/SignUpForm.tsx
@@ -1,148 +1,123 @@
-import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
+import React, { useState } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 const SignupForm = () => {
-  const handleSubmit = (values: any, { setSubmitting }: any) => {
-    setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      setSubmitting(false);
-    }, 400);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignUp = async (event: any) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
   };
 
-  const validationSchema = Yup.object().shape({
-    firstName: Yup.string().required("First name is required"),
-    lastName: Yup.string().required("Last name is required"),
-    email: Yup.string()
-      .email("Invalid email address")
-      .required("Email is required"),
-    password: Yup.string()
-      .min(6, "Password must be at least 6 characters")
-      .required("Password is required"),
-    confirmPassword: Yup.string()
-      .oneOf([Yup.ref("password"), null], "Passwords must match")
-      .required("Confirm password is required"),
-  });
-
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col justify-center sm:py-12">
+    <div className="bg-gray-100 min-h-screen flex flex-col justify-center sm:py-12 transition-all ease-in-out">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <h1 className="text-3xl font-bold mb-6">Sign up for an account</h1>
-          <Formik
-            initialValues={{
-              firstName: "",
-              lastName: "",
-              email: "",
-              password: "",
-              confirmPassword: "",
-            }}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <div className="mb-4">
-                  <label
-                    htmlFor="firstName"
-                    className="block text-gray-700 font-bold mb-2"
-                  >
-                    First Name:
-                  </label>
-                  <Field
-                    type="text"
-                    name="firstName"
-                    placeholder="First Name"
-                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  />
-                  <ErrorMessage
-                    name="firstName"
-                    className="text-red-500 text-sm italic mt-1"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label
-                    htmlFor="lastName"
-                    className="block text-gray-700 font-bold mb-2"
-                  >
-                    Last Name:
-                  </label>
-                  <Field
-                    type="text"
-                    name="lastName"
-                    placeholder="Last Name"
-                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  />
-                  <ErrorMessage
-                    name="lastName"
-                    className="text-red-500 text-sm italic mt-1"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label
-                    htmlFor="email"
-                    className="block text-gray-700 font-bold mb-2"
-                  >
-                    Email:
-                  </label>
-                  <Field
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  />
-                  <ErrorMessage
-                    name="email"
-                    className="text-red-500 text-sm italic mt-1"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label
-                    htmlFor="password"
-                    className="block text-gray-700 font-bold mb-2"
-                  >
-                    Password:
-                  </label>
-                  <Field
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  />
-                  <ErrorMessage
-                    name="password"
-                    className="text-red-500 text-sm italic mt-1"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label
-                    htmlFor="confirmPassword"
-                    className="block text-gray-700 font-bold mb-2"
-                  >
-                    Confirm Password:
-                  </label>
-                  <Field
-                    type="password"
-                    name="confirmPassword"
-                    placeholder="Confirm Password"
-                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  />
-                  <ErrorMessage
-                    name="confirmPassword"
-                    className="text-red-500 text-sm italic mt-1"
-                  />
-                </div>
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >
-                  Sign Up
-                </button>
-              </Form>
-            )}
-          </Formik>
+          <form onSubmit={handleSignUp}>
+            <div className="mb-4">
+              <label
+                htmlFor="firstName"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                First Name:
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                value={firstName}
+                onChange={(event) => setFirstName(event.target.value)}
+                placeholder="First Name"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="lastName"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Last Name:
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                value={lastName}
+                onChange={(event) => setLastName(event.target.value)}
+                placeholder="Last Name"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Email:
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                placeholder="Email"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Password:
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                placeholder="Password"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="confirmPassword"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Confirm Password:
+              </label>
+              <input
+                type="password"
+                name="confirmPassword"
+                value={confirmPassword}
+                onChange={(event) => setConfirmPassword(event.target.value)}
+                placeholder="Confirm Password"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="relative">
+              <div className="mb-4 text-red-500 text-sm italic absolute">
+                {error}
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-14 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Sign Up
+            </button>
+          </form>
         </div>
       </div>
     </div>
